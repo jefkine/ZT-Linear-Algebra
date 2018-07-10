e6405eb7bfc135b93a101b0ab1c4871d08a59678
jefkine <jefkine@gmail.com>
2018-07-10 07:59:50 +0300

    matrix operation examples

--- main.cpp
+++ main.cpp
@@ -27,52 +27,86 @@
 
 int main() {
 
-    // vector manipulations
+  double scalar = 2.0;
+  double scalar_result = 0.0;
 
-    double scalar = 2.0;
-    double scalar_result = 0.0;
+  // Vector Operations
 
-    std::vector<double> x = {1.0,2.0,3.0};
-    std::vector<double> y = {5.0,6.0,7.0};
-    std::vector<double> result = {};
+  std::vector<double> x = {1.0,2.0,3.0};
+  std::vector<double> y = {5.0,6.0,7.0};
+  std::vector<double> vec_result;
 
-    ZTVector<double> vec_x(x);
+  // get ZTVector vector object
+  ZTVector<double> vec_x(x);
 
-    // perfom vector to scalar addition
-    // result = vec_x.add(scalar);
-    // result = vec_x + scalar;
+  // perfom vector to scalar addition
+  // vec_result = vec_x.add(scalar);
+  // vec_result = vec_x + scalar;
 
-    // perfom vector to scalar subtraction
-    // result = vec_x.minus(scalar);
-    // result = vec_x - scalar;
+  // perfom vector to scalar subtraction
+  // vec_result = vec_x.minus(scalar);
+  // vec_result = vec_x - scalar;
 
-    // perfom vector to scalar multiplication
-    // result = vec_x.multiply(scalar);
-    // result = vec_x * scalar;
+  // perfom vector to scalar multiplication
+  // vec_result = vec_x.multiply(scalar);
+  // vec_result = vec_x * scalar;
 
-    // perfom vector to vector addition
-    // result = vec_x.add(y);
-    // result = vec_x + y;
+  // perfom vector to vector addition
+  // vec_result = vec_x.add(y);
+  // vec_result = vec_x + y;
 
-    // perfom vector to vector subtraction
-    // result = vec_x.minus(y);
-    // result = vec_x - y;
+  // perfom vector to vector subtraction
+  // vec_result = vec_x.minus(y);
+  // vec_result = vec_x - y;
 
-    // perfom vector to vector multiplication
-    // result = vec_x.multiply(y);
-    // result = vec_x * y;
+  // perfom vector to vector multiplication
+  // vec_result = vec_x.multiply(y);
+  // vec_result = vec_x * y;
 
-    // perfom vector norm
-    // scalar_result = vec_x.norm(y);
-    // scalar_result = vec_x.norm(x);  // norm with itself
-    // scalar_result = vec_x.norm();  // norm with itself
+  // perfom vector norm
+  // scalar_result = vec_x.norm(y);
+  // scalar_result = vec_x.norm(x);  // norm with itself
+  // scalar_result = vec_x.norm();  // norm with itself
 
-    // perfom vector dot operation
-    // scalar_result = vec_x.dot(y);
+  // perfom vector dot operation
+  // scalar_result = vec_x.dot(y);
 
-    ZTMatrix<double> mat(3, 5, 2.2);
-    ZTMatrix<double> res(3, 3, 2.0);
+  // Matrix Operations
 
-    return 0;
+  // get ZTMatrix matrix objects
+  ZTMatrix<double> X(3, 3, 2.2);
+  ZTMatrix<double> Y(3, 3, 2.0);
+
+  ZTMatrix<double> mat_result(3, 3, 0.0);
+
+  // perfom matrix to scalar addition
+  // mat_result = X.add(scalar);
+  // mat_result = X + scalar;
+
+  // perfom matrix to scalar subtraction
+  // mat_result = X.minus(scalar);
+  // mat_result = X - scalar;
+
+  // perfom matrix to scalar multiplication
+  // mat_result = X.multiply(scalar);
+  // mat_result = X * scalar;
+    
+  // perfom matrix to matrix addition
+  // mat_result = X.add(Y);
+  // mat_result = X + Y;
+
+  // perfom matrix to matrix subtraction
+  // mat_result = X.minus(Y);
+  // mat_result = X - Y;
+
+  // perfom matrix to matrix multiplication
+  // mat_result = X.multiply(Y);
+  // mat_result = X * Y;
+  
+  // perfom matrix trace and norm
+  // std::cout <<  X.trace() << std::endl;
+  // std::cout <<  X.norm() << std::endl;
+
+  return 0;
 
 }
